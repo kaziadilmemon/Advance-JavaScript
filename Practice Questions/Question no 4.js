1af9587b38a1e70f4182e8e78be7fd89f723c269
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-05-05 17:16:36 +0500

    Create Question no 4.js

---
+++ Practice Questions/Question no 4.js
@@ -0,0 +1,7 @@
+/* Question no : 4 Write a simple interest Calculator using JavaScript */
+
+function simpleInterest(p, r, t) {
+  return (p * r * t) / 100;
+}
+
+console.log(simpleInterest(100, 5, 1))
